06f45af4624ca451256a64be208af8b519c29979
iosumayor <iosumayor23@gmail.com>
2024-12-26 17:46:17 +0100

    Update-> Simplificacion breadCrumb

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { Link,useParams } from "react-router-dom";
 import { APP_TEXTS } from "constants/textPaths";
 import "styles/BreadcrumbsStyles.css"
 
 const Breadcrumbs: React.FC = () => {
-  const location = useLocation();
   const { id } = useParams<{ id: string }>(); 
-
-  const pathSegments = location.pathname.split("/").filter((path) => path);
 
   return (
     <nav className="breadcrumbs">
@@ -15,18 +12,6 @@ const Breadcrumbs: React.FC = () => {
         <li>
           <Link to="/">{APP_TEXTS.BREADCRUMBS_PRINCIPAL}</Link>
         </li>
-        {pathSegments.map((segment, index) => {
-          const path = `/${pathSegments.slice(0, index + 1).join("/")}`;
-          const isLast = index === pathSegments.length - 1;
-
-          if (segment === "product" && isLast && id) {
-            return (
-              <li key={path}>
-                <span>{APP_TEXTS.BREADCRUMB_DETAIL}</span>
-              </li>
-            );
-          }
-        })}
         {id && (
           <li>
             <Link to={`/product/${id}`}>{APP_TEXTS.BREADCRUMB_DETAIL}</Link>
